ec531c2a9b2dbf8b443ee3bca5b1a677441398d3
Lorenis-57 <umaloucanouniverso@gmail.com>
2024-10-29 11:50:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geek',
+    'Otaku é gente?',
+    'Não'
+)
+
+criaCartao(
+    'Geral',
+    'O que acontece com as encomendas quando caem no oceano?',
+    'Molha'
+)
+
+criaCartao(
+    'Cultura',
+    'Vc sabe o que é caviar?',
+    'Nunca vi nem comi eu só ouço falar'
+)
+
+criaCartao(
+    'Publi',
+    'Hotel?',
+    'Trivago'
+)
